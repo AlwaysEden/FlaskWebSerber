fd949e36ce87a7d0a8c26a0cf8ede4f602ec59dc
Chodabeen <82192889+Chodabeen@users.noreply.github.com>
2024-06-02 21:00:18 +0900

    update client.py

--- client.py
+++ client.py
@@ -1,32 +1,32 @@
 import requests
 
-# # 서버 URL과 엔드포인트
-# url = "http://127.0.0.1:5000/status/changeEquipment"
-
-# # 요청할 때 보낼 파라미터
-# params = {
-#     "userID": "user123", 
-#     "equipID" : 102
-# }
-
-# # POST 요청 보내기
-# response = requests.post(url, params=params)
-
-# # 응답 내용 출력
-# print(response.json())
-
-
-
-
-url = "http://127.0.0.1:5000/status/inventory"
+# 서버 URL과 엔드포인트
+url = "http://127.0.0.1:5000/status/changeEquipment"
 
 # 요청할 때 보낼 파라미터
 params = {
-    "userID": "user123"
+    "userID": "user123", 
+    "equipID" : 102
 }
 
-# GET 요청 보내기
-response = requests.get(url, params=params)
+# POST 요청 보내기
+response = requests.post(url, params=params)
 
 # 응답 내용 출력
 print(response.json())
+
+
+
+
+# url = "http://127.0.0.1:5000/status/inventory"
+
+# # 요청할 때 보낼 파라미터
+# params = {
+#     "userID": "user123"
+# }
+
+# # GET 요청 보내기
+# response = requests.get(url, params=params)
+
+# # 응답 내용 출력
+# print(response.json())
